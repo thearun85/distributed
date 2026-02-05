2d15659288af3e6424eaeb58bffe6ad480ca2b11
Arun Raghunath <arun2aruns@gmail.com>
2026-02-05 09:56:18 +0000

    SWIM - port included to a node instance

--- protocols/swim/node.py
+++ protocols/swim/node.py
@@ -4,22 +4,24 @@ SWIM - Distributed membership protocol - gossip/ infection style implementation
 
 
 class Node:
-    def __init__(self, node_id: str):
+    def __init__(self, node_id: str, port: int):
         """Initialize a SWIM node instance"""
         self.node_id = node_id
+        self.port = port
 
     def start(self):
         """Start the SWIM node instance"""
-        print(f"{self.node_id} started")
+        print(f"{self.node_id} started on port {self.port}")
 
 
 
 if __name__ == '__main__':
     import sys
-    if len(sys.argv) < 2:
-        print("Usage: python node.py <node_id>")
+    if len(sys.argv) < 3:
+        print("Usage: python node.py <node_id> <port>")
         sys.exit(1)
 
     node_id = sys.argv[1]
-    node = Node(node_id)
+    port = int(sys.argv[2])
+    node = Node(node_id, port)
     node.start()
