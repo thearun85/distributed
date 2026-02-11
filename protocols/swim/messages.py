6ecf5f599160c3c9257b033afbd73c82709b1c43
Arun Raghunath <arun2aruns@gmail.com>
2026-02-11 10:47:24 +0000

    Create PING_REQ and enhance ACK to handle indirect pings

--- protocols/swim/messages.py
+++ protocols/swim/messages.py
@@ -14,11 +14,12 @@ def create_ping(sender: str, host:str, port: int, sequence: int):
         "sequence": sequence,
     })
 
-def create_ack(sender: str, sequence: int):
+def create_ack(sender: str, sequence: int, on_behalf_of: str = None:
     return json.dumps({
         "type": "ACK",
         "sender": sender,
         "sequence": sequence,
+        "on_behalf_of": on_behalf_of, # in case of PING_REQ
     })
 
 def parse_message(message: str):
@@ -30,3 +31,16 @@ def parse_message(message: str):
     except Exception as e:
         logging.error(f"[parse_message] Exception: {e}")
         return None
+
+def create_ping_req(sender: str, host: str, port: int, sequence: int,
+                target_node: str, target_host: str, target_port: int):
+    return json.dumps({
+        "type": "PING_REQ",
+        "sender": sender,
+        "host": host,
+        "port": port,
+        "sequence": sequence,
+        "target_node": target_node,
+        "target_host": target_host,
+        "target_port": target_port,
+    })
